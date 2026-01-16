bf6d43c751ac7b35f9f6f6e00ab686bd143bbb03
ajinkay103 <bramhankar103@gmail.com>
2026-01-16 23:49:44 +0530

    init commit

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+class demo
+{ public static void main (String[]args)
+ 
+{int a=23;
+int b=45;
+a++;
+--b;
+System.out.println(a);
+System.out.println(a);
+}
+}
---
+++ demo.java
@@ -0,0 +1,10 @@
+class demo
+public static void main(string[]args);
+{
+	int a=33;
+	int b=86;
+	System.out.println(a++ - b--);
+	System.out.println(a-- + b--);
+	System.out.println a;
+	System.out.println b;
+}	
